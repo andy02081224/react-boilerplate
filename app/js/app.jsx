ff2ecb6d5feb75f3b9980a1c1031765bb4d75a04
andy02081224 <andy02081224@gmail.com>
2016-05-03 23:56:03 +0800

    Add SCSS partial structure and the main SCSS file
    Add normalize and sass-mq as required vendor files
    Add some styles to show the basic usage of the structure

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import HelloWorld from './components/HelloWorld/HelloWorld.jsx';
 
+import '../styles/app.scss';
+
 let test = (...args) => {
 	console.log(args);
 };
